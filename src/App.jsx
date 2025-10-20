0ce14df30cfd0130d18f77d35493dd00b63fe343
Andres Ponce <1693035.richie8726.users.noreply.github.com>
2025-10-20 16:07:52 -0300

    Subiendo proyecto actual para revisión Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
-import Header from "./components/Header";
-import TaskList from "./components/TaskList";
-import Footer from "./components/Footer";
-
-function App() {
+export default function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50 px-4">
-      <div className="w-full max-w-md bg-white rounded-2xl shadow-lg p-6">
-        <Header />
-        <TaskList />
-        <Footer />
-      </div>
-    </div>
+    <h1 className="text-4xl font-bold text-blue-500">
+      ¡Tailwind funcionando! 🎉
+    </h1>
   );
 }
-
-export default App;
